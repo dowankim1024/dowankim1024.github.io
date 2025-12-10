49782574a908a7c4134b3228b6b11c0d563645e0
kimdowan <kimdowan1004@naver.com>
2025-12-10 09:52:50 +0900

    fix: tailwind 기본 spacing scale에 18 없는 문제로 명확한 크기 지정

--- components/Home/ArrowUp/ArrowUp.tsx
+++ components/Home/ArrowUp/ArrowUp.tsx
@@ -31,7 +31,7 @@ export default function ArrowUp() {
   return (
     <aside>
       <Link
-        className="fixed bottom-12 right-12 z-[1000] text-5xl w-18 h-18 rounded-full text-center bg-[#050a13] shadow-[0_3px_10px_#03e8f9] transition-opacity duration-300 text-white flex items-center justify-center max-md:text-3xl max-md:w-12 max-md:h-12 max-md:bottom-6 max-md:right-6"
+        className="fixed bottom-12 right-12 z-[1000] text-5xl w-[72px] h-[72px] rounded-full text-center bg-[#050a13] shadow-[0_3px_10px_#03e8f9] transition-opacity duration-300 text-white flex items-center justify-center max-md:text-3xl max-md:w-12 max-md:h-12 max-md:bottom-6 max-md:right-6"
         href="#home"
         title="back to top"
         onClick={scrollToTop}
